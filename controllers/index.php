eae0755cc8c9aec4c6df2627ba2970a4c25e8ea4
Didier Prolhac <dev@thepozer.net>
2017-03-03 16:10:57 +0100

    Add Preview of the last status on the index page

--- controllers/index.php
+++ controllers/index.php
@@ -2,6 +2,20 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+require 'data/servers.php';
+
 $oGlobalApp->get('/', function (Request $oRequest, Response $oResponse, $args) {
+    $sStatusFile = 'data/status.json';
+    
+    $this->view->arStatus = json_decode(file_get_contents($sStatusFile), true);
+    
     return $this->view->render($oResponse, 'index');
+});
+
+$oGlobalApp->get('/config', function (Request $oRequest, Response $oResponse, $args) use ($arServers) {
+    $sStatusFile = 'data/status.json';
+    
+    $this->view->arServers = $arServers;
+    
+    return $this->view->render($oResponse, 'config');
 });
--- scripts/check.php
+++ scripts/check.php
@@ -3,13 +3,17 @@ require '_bootstrap.php';
 
 require 'data/servers.php';
 
-$arResults = ['#General' => ['date' => date('Y-m-d H:i:s')]];
+$iCurrTime = time();
+
+$sDirName = 'data/checks/' . date('Y', $iCurrTime) . '/' . date('m', $iCurrTime) . '/' . date('d', $iCurrTime) . '/' . date('H', $iCurrTime);
+$sFileName = $sDirName . '/status-' . date('Y-m-d-Hi', $iCurrTime) . '.json';
+$arResults = ['#General' => ['date' => date('Y-m-d H:i:s', $iCurrTime)]];
 
 foreach($arServers as $sHostName => $arServer) {
-    echo "Looking for {$sHostName}\n";
+    debug("Looking for {$sHostName}");
 
     $oHost = Host::getInstance($sHostName, $arServer);
-    $arResults[$sHostName] = [];
+    $arResults[$sHostName] = ['#status' => 'OK'];
 
     foreach($arServer['services'] as $sServiceName => $arParams) {
         try {
@@ -18,10 +22,21 @@ foreach($arServers as $sHostName => $arServer) {
             $arRet = $oCheck->check($oHost, $arParams);
 
             $arResults[$sHostName] = array_merge($arResults[$sHostName], $arRet);
+            
+            foreach($arRet as $arService) {
+                 $arResults[$sHostName]['#status'] = ($arService['status'] === 'KO') ? 'KO' : $arResults[$sHostName]['#status'] ;
+            }
         } catch (Exception $e) {
-            echo "Exception : $e\n";
+            error("Exception : $e");
         }
     }
 }
 
-print_r($arResults);
+debug("sDirName  : '{$sDirName}'");
+debug("sFileName : '{$sFileName}'");
+
+if (!is_dir($sDirName)) {
+    mkdir($sDirName, 0777, true);
+}
+file_put_contents($sFileName, json_encode($arResults));
+file_put_contents('data/status.json', json_encode($arResults));
